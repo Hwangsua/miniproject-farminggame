c4b32d6d8e1a0618ee6fa63522f38ff26c83610a
Hwangsua <76787224+Hwangsua@users.noreply.github.com>
2021-06-20 19:54:57 +0900

    Update SoundsClip.java

--- miniproject-farminggame/src/miniproject_game/SoundsClip.java
+++ miniproject-farminggame/src/miniproject_game/SoundsClip.java
@@ -5,31 +5,52 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
 /**
- * @author EunkyungHwang
- *
+ * 
+ * 음악 사운드 출력을 위한 클래스
+ * 
+ * @author EunKyungHwang
+ * 
  */
 public class SoundsClip {
-	   static final String PLANTING_SOUND= "music\\planting.wav"; // 심을 때
-	   static final String PURCHASE_SOUND= "music\\purchase.wav"; // 구매
-	   static final String SUCCESS_SOUND= "music\\success.wav"; // 업그레이드 성공, 밭 확장 
-	   static final String CLICK_SOUND = "music\\click.wav"; // 클릭했을 때
-	   static final String BACKGROUND_MUSIC =  "music\\backgroundMusic.wav";
-	   
-	   
-	   static  final void play(String fileName)
-	    {
-	        try
-	        {
-	            AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
-	            Clip clip = AudioSystem.getClip();
-	            clip.stop();
-	            clip.open(ais);
-	            clip.start();
-	        }
-	        catch (Exception ex)
-	        {
-	        }
-	    }
+	/**
+	 * 심는 소리
+	 */
+	static final String PLANTING_SOUND = "music\\planting.wav";
+	/**
+	 * 구매소리
+	 */
+	static final String PURCHASE_SOUND = "music\\purchase.wav";
+	/**
+	 * 업그레이드및 밭 확장소리
+	 */
+	static final String SUCCESS_SOUND = "music\\success.wav";
+	/**
+	 * 클릭 소리
+	 */
+	static final String CLICK_SOUND = "music\\click.wav";
+	/**
+	 * 배경소리
+	 */
+	static final String BACKGROUND_MUSIC = "music\\backgroundMusic.wav";
 
+	/**
+	 * 
+	 * 음악파일을 불러와 음악을 실행시킨다.
+	 * 
+	 * @param fileName 사용할 음악파일이름
+	 * @since 1.0
+	 */
+	static final void play(String fileName) {
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
+			Clip clip = AudioSystem.getClip();
+			clip.stop();
+			clip.open(ais);
+			clip.start();
+		} catch (Exception ex) {
+		}
 	}
+
+}
