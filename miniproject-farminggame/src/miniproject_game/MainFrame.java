fb40222f2faf31ba86c8fd1ae165f57ad591b19e
Hwangsua <76787224+Hwangsua@users.noreply.github.com>
2021-06-20 19:50:05 +0900

    Update MainFrame.java

--- miniproject-farminggame/src/miniproject_game/MainFrame.java
+++ miniproject-farminggame/src/miniproject_game/MainFrame.java
@@ -9,17 +9,26 @@ import javax.swing.Icon;
 import javax.swing.JFrame;
 
 /**
- * 본 프로그램의 Main 프레임<br>
+ * MainFrame <br/>
+ * 본 프레임과 다른 객체들을 연결하는 컨트롤러역할을 한다.<br/>
+ * WindowListener를 포함한다.
+ * 
  * @author SuaHwang
  * @author EunkyungHwang
  *
  */
 
-public class MainFrame extends JFrame implements Serializable, WindowListener{
+public class MainFrame extends JFrame implements Serializable, WindowListener {
 
 	final String userID;
 
+	/**
+	 * MainFrame 의 폭
+	 */
 	public static final int MAIN_FRAME_WIDTH = 1280;
+	/**
+	 * MainFrame 의 넓이
+	 */
 	public static final int MAIN_FRAME_HEIGHT = 720;
 
 	FarmingPanel farmingField;
@@ -27,8 +36,14 @@ public class MainFrame extends JFrame implements Serializable, WindowListener{
 
 	private static String mainSeedName;
 
+	/**
+	 * MainFrame 의 생성자<br>
+	 * WindowListener를 add().
+	 * 
+	 * @param userID
+	 */
 	MainFrame(String userID) {
-		
+
 		this.userID = userID;
 		farmingField = new FarmingPanel(this);
 		menuPanel = new MenuPanel(this);
@@ -46,6 +61,9 @@ public class MainFrame extends JFrame implements Serializable, WindowListener{
 		this.addWindowListener(this);
 	}
 
+	/**
+	 * FarmingPanel 및 MenuPanel을 MainFrame 에 add().
+	 */
 	private void initializeComponent() {
 
 		farmingField.setBounds(0, 0, FarmingPanel.FARMING_PANEL_WIDTH, MAIN_FRAME_HEIGHT);
@@ -54,49 +72,99 @@ public class MainFrame extends JFrame implements Serializable, WindowListener{
 		add(farmingField);
 		add(menuPanel);
 
-		// 메뉴 1,3 클릭시 마우스리스너구현
 		menuPanel.setMenu01Mouse(menuPanel.menu1, menuPanel.enterseedIcon, menuPanel.seedIcon);
 		menuPanel.setMenu3Mouse(menuPanel.menu3, menuPanel.enterseedIcon);
 
 		this.setLocation(new Point(0, 0));
 	}
 
+	/**
+	 * 
+	 * {@link FarmingPanel} 객체에 대한 user의 레벨을 반환한다.
+	 * 
+	 * @return user의레벨
+	 */
 	public int getUserLevel() {
 		return farmingField.getUserLevel();
 	}
 
-	public boolean checkCoin(int cost) { // 값을 지불할 수 있는지 확인
-		if (farmingField.getCoin() < cost) { // 코인 부족할 경우 false
+	/**
+	 * 
+	 * {@link FarmingPanel} 객체에 대한 coin이 충분한지 확인한다.
+	 * 
+	 * @param cost 비용(지출 코인)
+	 * @return cost를 지불할 coin이 부족하면 false, 그렇지 않으면 true
+	 */
+	public boolean checkCoin(int cost) {
+		if (farmingField.getCoin() < cost) {
 			FarmerMessage.getinsufficientCoinMsg();
 			return false;
 		}
 		return true;
 	}
 
-	public void reduceCoin(int cost) { // 값을 지불
+	/**
+	 * 
+	 * {@link FarmingPanel} 객체에 대한 coin을 계산한다.
+	 * 
+	 * @param cost 비용(수입/지출 될 코인)
+	 */
+	public void reduceCoin(int cost) {
 		farmingField.calculateCoin(-cost);
 	}
 
-	public void setPlantSeed(String string) { // 씨앗 이름 set
+	/**
+	 * 
+	 * MainFrame 에서 씨앗 이름을 설정한다.
+	 * 
+	 * @param string 씨앗이름
+	 */
+	public void setPlantSeed(String string) {
 		mainSeedName = string;
 	}
 
+	/**
+	 * 
+	 * MainFrame에서 설정한 씨앗 이름을 반환환다.
+	 * 
+	 * @return 씨앗이름
+	 */
 	public String getPlantSeed() {
 		return this.mainSeedName;
 	}
 
-	public void createField(int col) { // 밭 생성
+	/**
+	 * {@link FarmingPanel} 객체에 메소드를 이용하여 밭을 추가로 생성한다.
+	 * 
+	 * @param col 추가할 밭의 열
+	 */
+	public void createField(int col) {
 		farmingField.addField(col);
+	} 
+
+	/**
+	 * 
+	 * {@link FarmingPanel} 객체에 메소드를 이용하여 스킬을 실행한다.
+	 * 
+	 * @param skillLevel 스킬레벨
+	 */
+	public void setWaterLevel(int skillLevel) {
+		farmingField.runWaterSkill(skillLevel);
 	}
 
-	public void setWaterLevel(int skillLevel) { // 물 스킬레벨 set
-		farmingField.runWaterSkill(skillLevel); // 물 스킬 실행
-	}
-
+	/**
+	 * 
+	 * {@link MenuPanel} 객체에 이미지 아이콘을 반환한다.
+	 * 
+	 * @return enterseedIcon 이미지아이콘
+	 */
 	public Icon getEnterseedIcon() {
 		return menuPanel.enterseedIcon;
 	}
 
+	/**
+	 * 윈도우창을 열 때 {@link FileUtils}의 파일 load 메소드를 실행한다.
+	 */
 	@Override
 	public void windowOpened(WindowEvent e) {
 		FileUtils.load(this, userID);
@@ -122,9 +190,12 @@ public class MainFrame extends JFrame implements Serializable, WindowListener{
 	public void windowDeactivated(WindowEvent e) {
 	}
 
+	/**
+	 * 윈도우 창 종료시 {@link FileUtils}의 파일 save 메소드를 실행한다.
+	 */
 	@Override
 	public void windowClosing(WindowEvent e) {
-		FileUtils.save(this,userID);
+		FileUtils.save(this, userID);
 	}
 
 }
